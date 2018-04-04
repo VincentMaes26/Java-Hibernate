1541f329f6433ec2d1e6aedb4a3430dadbb2908e
VincentMaes26 <32563747+VincentMaes26@users.noreply.github.com>
2018-04-04 19:25:08 +0200

    First working version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.Session;
 
@@ -17,6 +18,45 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	// write your code here
+
+        Main jpa = new Main();
+        try {
+            jpa.setUp();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
+
+        public void setUp() throws Exception {
+            Session session = factory.openSession();
+            Transaction tx = session.beginTransaction();
+
+            try{
+                Student student1 = new Student("Vincent", "Maes", "vincent-maes-@hotmail.com", "+32474778073");
+                session.save(student1);
+
+                MarksDetails marksDetails1 = new MarksDetails("English", 20, 16, "Pass");
+                marksDetails1.setStudent(student1);
+                session.save(marksDetails1);
+
+                MarksDetails marksDetails2 = new MarksDetails("Java Advanced", 20, 14, "Pass");
+                marksDetails2.setStudent(student1);
+                session.save(marksDetails2);
+
+                tx.commit();
+                session.close();
+                System.out.println("The records have been successfully added to the database");
+            }
+            catch (Exception e){
+                if (tx != null) {
+                    tx.rollback();
+                }
+                System.out.println(e.getMessage());
+            }
+            finally {
+                session.close();
+            }
+        }
+
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
+import java.util.Set;
 
 @Entity
 @Table(name = "student")
